612516e46a96608cba0466f1539eb3d7d9117a0c
Elsaterziu <Et70869@ubt-uni.net>
2026-01-21 02:40:14 +0100

    database connection setup

---
+++ FinalsTrack/FinalsTrack.Api/Data/FinalsTrackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinalsTrack.Api.Data
+{
+    public class FinalsTrackDbContext : DbContext
+    {
+        public FinalsTrackDbContext(DbContextOptions<FinalsTrackDbContext> options)
+            : base(options)
+        {
+        }
+
+       
+    }
+}
